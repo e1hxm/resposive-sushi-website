01eb3cc012049325b22618a3a65509a02e92ae33
El'ham <elham@ro.ru>
2025-01-12 23:41:42 +0300

    Доделан header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,32 @@
 /*=============== SHOW MENU ===============*/
+let navMenu   = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose  = document.getElementById('nav-close')
 
+/*====== SHOW MENU ======*/
+
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== HIDDEN MENU =====*/
+
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+let navLink = document.querySelectorAll('.nav__link')
 
+let linkAction = () => {
+    let navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
