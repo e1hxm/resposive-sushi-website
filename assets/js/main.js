a3c46c8d4a819c3640cae6321f45d849de36e734
El'ham <elham@ro.ru>
2025-02-09 22:26:02 +0300

    Доделана мобильная версия, с javascript кодом для меню

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,10 +38,33 @@ let scrollHeader = () => {
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+let scrollUp = () => {
+    let scrollUp = document.getElementById('scroll-up')
+    this.scrollY>= 350 ? scrollUp.classList.add('show-scroll')
+                       : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== DARK LIGHT THEME ===============*/ 
 
